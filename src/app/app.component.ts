251adec116d2e9aa5ba6bb95aaa4e06367f89ded
mohammadghaliah <m.ghaliah98@gmail.com>
2022-08-14 22:03:46 +0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,15 @@ import { Component, VERSION } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  email: any;
+  onSubmit(contactForm) {
+    // if (contactForm.valid) {
+    console.log(contactForm.value);
+    // }
+  }
+
+  click() {
+    console.log('click');
+  }
+}
